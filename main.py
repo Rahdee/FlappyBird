5285aaf4a391f3a31c2940c33a5c2d11062593f4
Rahdee <4283301@myuwc.ac.za>
2023-06-27 08:04:09 +0200

    added jump motion

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame
+import random
 from pygame.locals import *
 
 pygame.init()
-window = pygame.display.set_mode((288,512))
+screen_width = 288
+screen_height = 512
+window = pygame.display.set_mode((screen_width,screen_height))
 fps = pygame.time.Clock()
 
 # Declare variables
@@ -10,12 +13,18 @@ BACKGROUND = pygame.image.load("media/background.png").convert_alpha()
 BASE = pygame.image.load("media/base.png").convert_alpha()
 BOTTOM_PIPE = pygame.image.load("media/pipe.png").convert_alpha()
 TOP_PIPE = pygame.transform.flip(BOTTOM_PIPE, False, True)
+BIRD =pygame.image.load("media/flappybird-animation.png").convert_alpha()
 pipe_height = 320
+pipe_width = 52
 pipe_gap = 100
+pipe_speed = 10
 
 finished = False
-FLAP = False
+flap = False
 pipe = [200, 100] # X location of both pipes . Y location of top of gap
+bird = [50, 250]
+bird_anime_count = 0
+jump = 0
 
 #Main game LookupError
 while not finished:
@@ -31,6 +40,17 @@ while not finished:
 
 
         #game logic
+        if flap:
+            jump = 18
+            flap = False
+        bird[1] -= jump
+        if jump > -12:
+            jump -= 2
+        pipe[0] -= pipe_speed
+        if pipe[0] < -pipe_width:
+            pipe[0] = screen_width
+            pipe[1] = random.randint(50, screen_height-150)
+            pipe_gap = random.randint(100,200)
 
         #draw graphics
         window.fill((0,0,0))
@@ -41,6 +61,9 @@ while not finished:
         window.blit(TOP_PIPE, (pipe[0], pipe[1]-pipe_height))
         window.blit(BOTTOM_PIPE, (pipe[0], pipe[1]+pipe_gap))
         #FlappyBird
+        window.blit(BIRD, bird, (0,bird_anime_count*24,34,24))
+        bird_anime_count = (bird_anime_count + 1) % 3
+
         pygame.display.update()
         fps.tick(25)
 
